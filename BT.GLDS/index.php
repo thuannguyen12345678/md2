b01795e21d741b72d8d053a60b8325af79775fac
taitamfc <taitam8590@gmail.com>
2022-05-30 08:09:00 +0700

    Update index.php

--- BT.GLDS/index.php
+++ BT.GLDS/index.php
@@ -4,15 +4,43 @@ include_once "MODEL/EmployeeManager.php";
 
   use INFORMATION\EmployeeManager;
   use INFORMATION\Employee;
+
+  $emp_1 = new Employee("Nguyễn Đức", "Thuần", "20_3_1998", "đông hà", "Giám đốc");
+  $emp_2 = new Employee("Nguyễn Đức", "Vinh", "20_3_1998", "đông hà", "Tong Giám đốc");
+  $emp_3 = new Employee("Nguyễn Đức", "Lan", "20_3_1998", "đông hà", "Ke Toan");
  
   $employeeManager = new EmployeeManager();
-  $employeeManager->add(new Employee("nguyen"));
-  $employeeManager->add(new Employee("thuan"));
-  $employeeManager->add(new Employee("20_3_1998"));
-  $employeeManager->add(new Employee("dong_ha"));
-  $employeeManager->add(new Employee("nhan_vien"));
+  
+  $employeeManager->add( $emp_1 );
+  $employeeManager->add( $emp_2 );
+  $employeeManager->add( $emp_3 );
+  $employeeManager->add( new Employee("Nguyễn Đức", "Huong", "20_3_1998", "đông hà", "Ke Toan") );
+
+  echo '<pre>';
+  print_r($employeeManager);
+  echo '</pre>';
+
 
   $employees = $employeeManager->getEmployees();
+
+
+  echo '<pre>';
+  print_r($employees);
+  echo '</pre>';
+
+ /*
+    Array[
+       Employee {
+        ho -> 
+        ten ->
+        ...
+       },
+       Employee {
+        
+       }
+    ]
+ */
+
   foreach ($employees as $Employee) {
       echo $employee->getTen();
   }
